41004fd9343c0c8b6e472bb29163f8a9ed11d168
ImTaliesin <110573773+ImTaliesin@users.noreply.github.com>
2024-02-14 15:29:54 -0800

    Refactor backgroundSection and AboutMe components

--- src/components/Small/backgroundSection.tsx
+++ src/components/Small/backgroundSection.tsx
@@ -1,23 +1,25 @@
+import React from 'react';
 import SVGComponent from './svgComponent';
 
 interface SectionProps {
-	children: React.ReactNode; // Use `children` to accept any number of child components
+  children: React.ReactNode;
 }
 
 export default function BackgroundSection({ children }: SectionProps) {
-	// Define a custom style for the gradient overlay
-	const gradientOverlayStyle = {
-	  background: 'linear-gradient(to right, rgba(245, 245, 245, 0.8), rgba(245, 245, 245, 0.8))',
-	};
- 
-	return (
-	  <div className='overflow-hidden'>
-		 <SVGComponent className='w-full h-full absolute inset-0 z-0' />
-		 {/* Gradient Overlay */}
-		 <div style={gradientOverlayStyle} className='w-full h-full absolute inset-0 z-10' />
-		 {/* Content */}
-		 <div className='relative z-20'>{children}</div>
-	  </div>
-	);
- }
- 
+  return (
+    <div className='relative overflow-hidden'>
+      <SVGComponent className='absolute w-full h-full inset-0 z-0' />
+      {/* Gradient Overlay */}
+      <div
+        style={{
+          background: 'linear-gradient(rgba(245, 245, 245, 0.8), rgba(245, 245, 245, 0.8))',
+        }}
+        className='absolute w-full h-full inset-0 z-10'
+      />
+      {/* Content */}
+      <div className='relative z-20'>
+        {children}
+      </div>
+    </div>
+  );
+}
