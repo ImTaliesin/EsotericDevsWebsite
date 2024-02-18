0ec681c1afb45a495618097f52a42fcf313e51c9
ImTaliesin <110573773+ImTaliesin@users.noreply.github.com>
2024-02-17 17:22:58 -0800

    Refactor DarkModeToggle component

--- src/components/UI/DarkMode.tsx
+++ src/components/UI/DarkMode.tsx
@@ -1,43 +1,30 @@
 'use client';
 import { useTheme } from 'next-themes';
 import { useEffect, useState } from 'react';
-import MoonIcon from '../svg/moonSVG'; // Ensure this is the correct path to your Moon SVG component
-import SunIcon from '../svg/sunSVG'; // Ensure this is the correct path to your Sun SVG component
+import MoonIcon from '../svg/moonSVG'; // Adjust paths as necessary
+import SunIcon from '../svg/sunSVG'; // Adjust paths as necessary
 
 export default function DarkModeToggle() {
-	const { theme, setTheme } = useTheme();
-	const [mounted, setMounted] = useState(false);
+    const { theme, setTheme } = useTheme();
+    const [mounted, setMounted] = useState(false);
 
-	useEffect(() => {
-		setMounted(true);
-	}, []);
+    useEffect(() => {
+        setMounted(true);
+    }, []);
 
-	if (!mounted) return null;
+    if (!mounted) return null;
 
-	const toggleTheme = () => {
-		console.log(
-			'Toggling theme from:',
-			theme,
-			'to:',
-			theme === 'dark' ? 'light' : 'dark'
-		);
-		setTheme(theme === 'dark' ? 'light' : 'dark');
-	};
+    const toggleTheme = () => {
+        setTheme(theme === 'dark' ? 'light' : 'dark');
+    };
 
-	// Use SVG icons instead of text
-	const ThemeIcon =
-		theme === 'dark' ||
-		(!theme && window.matchMedia('(prefers-color-scheme: dark)').matches) ? (
-			<SunIcon />
-		) : (
-			<MoonIcon />
-		);
+    // Optimized theme icon determination
+    const isDarkMode = theme === 'dark' || (!theme && window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
+    const ThemeIcon = isDarkMode ? <SunIcon /> : <MoonIcon />;
 
-	return (
-		<button
-			onClick={toggleTheme}
-			className=' z-50 fixed bottom-28 right-2.5 flex justify-center items-center h-10 w-16 overflow-hidden transition-all duration-200 hover:text-white hover:shadow-violet-700'>
-			{ThemeIcon}
-		</button>
-	);
+    return (
+        <button onClick={toggleTheme} className="z-50 fixed lg:bottom-32 lg:right-7 bottom-28 right-2.5 flex justify-center items-center h-10 w-16 overflow-hidden transition-all duration-200 hover:text-white hover:shadow-violet-700">
+            {ThemeIcon}
+        </button>
+    );
 }
