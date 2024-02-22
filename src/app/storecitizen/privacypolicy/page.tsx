d19f10f7a46453f370e6aa873474ca8bd1bc236d
ImTaliesin <110573773+ImTaliesin@users.noreply.github.com>
2024-02-21 23:09:52 -0800

    Everything is fucked. send help

---
+++ src/app/storecitizen/privacypolicy/page.tsx
@@ -0,0 +1,33 @@
+export default function Privacy() {
+   return <div>Privacy Policy for Store Citizen
+
+   Last Updated: 2/21/24
+   
+   1. Introduction
+   
+   Welcome to Store Citizen. This privacy policy explains how we handle your personal information and data. We value your trust, so we’ve strived to present this policy in clear, plain language instead of legalese.
+   
+   2. Data Collection
+   
+   Store Citizen does not collect or store any personal information. The extension/app functions without the need for any user data.
+   
+   3. Data Usage
+   
+   Since no personal data is collected, there is no usage of such data.
+   
+   4. Third-Party Data Sharing
+   
+   No data is shared with third parties as we do not collect any data.
+   
+   5. Security
+   
+   Although we do not collect personal data, we are committed to ensuring the security and integrity of our extension/app.
+   
+   6. Changes to This Privacy Policy
+   
+   This Privacy Policy may change from time to time. We will post any privacy policy changes on this page.
+   
+   7. Contact Us
+   
+   If you have questions about this privacy policy, please contact me at Taliesin@esotericdevs.com.</div>
+}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-// Example critical CSS string
-// In a real project, this would be dynamically imported or defined elsewhere
 const criticalCss = `
   /* Your critical CSS here */
   body { margin: 0; font-family: 'League Spartan', sans-serif; }
@@ -15,43 +13,6 @@ class MyDocument extends Document {
         <Head>
           {/* Inline critical CSS */}
           <style dangerouslySetInnerHTML={{ __html: criticalCss }} />
-
-         
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-
-          
-          <link
-            rel="stylesheet"
-            href="//fonts.googleapis.com/css?family=Playfair+Display"
-            media="print"
-            onLoad={(event) => {
-					//access the currentTarget and set its media to 'all'
-					event.currentTarget.media = 'all';
-				 }}
-          />
-          <noscript>
-            <link
-              rel="stylesheet"
-              href="//fonts.googleapis.com/css?family=Playfair+Display"
-            />
-          </noscript>
-
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&display=swap"
-            media="print"
-            onLoad={(event) => {
-					//access the currentTarget and set its media to 'all'
-					event.currentTarget.media = 'all';
-				 }}
-          />
-          <noscript>
-            <link
-              rel="stylesheet"
-              href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&display=swap"
-            />
-          </noscript>
         </Head>
         <body>
           <Main />
