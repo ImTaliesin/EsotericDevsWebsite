00831b1dc530530bd8057aa124452d5dedaf26f4
ImTaliesin <110573773+ImTaliesin@users.noreply.github.com>
2024-03-07 14:22:47 -0800

    Refactor AboutMe component UI

--- src/components/UI/AboutMe.tsx
+++ src/components/UI/AboutMe.tsx
@@ -17,30 +17,40 @@ const AboutMe = () => {
 				</p>
 			</div>
 			{/* Below About Me Two Columns*/}
-			<div className='max-w-5xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-4 mb-10 px-4'>
-				{/* Left Column*/}
-				<div className='flex mx-auto my-2 h-1 w-12 bg-violet-600 rounded-full justify-center'></div>
-				<div className='flex-col text-center space-y-4 max-w-xl text-xl justify-center align-middle mx-auto'>
-					<p>
-						I'm a Full Stack Web Developer building Websites and Web
-						Applications that leads to the success of your product and business.
-						Check out some of my work in the Projects section.
-					</p>
-					<p>
-						I'm open to Job opportunities where I can contribute, learn and
-						grow. If you have a good opportunity that matches my skills then
-						don't hesitate to contact me.
-					</p>
-					<Button className='w-52 py-6 shadow-gray-600 shadow-lg bg-violet-700 text-white rounded text-2xl'>
-						Contact
-					</Button>
+			<div className='max-w-5xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-4 mb-6 px-4'>
+				{/* Purple Line */}
+				<div className='flex justify-center col-span-full'>
+					<div className='h-1 w-48 bg-violet-600 rounded-full'></div>
 				</div>
-				{/* Right Column*/}
-				<div className='flex flex-col items-center justify-self-center justify-top mt-4 space-y-4 mb-4 max-w-md '>
+
+				{/* Left Column */}
+				<div className='flex flex-col items-center justify-center mx-4'>
+					<div className='text-center space-y-4 max-w-xl text-xl md:pt-5'>
+						<p>
+							I'm a web developer who loves building websites and apps that help
+							businesses succeed. I've worked on all sorts of projects, from the
+							front-end stuff you see to the back-end code that makes it all
+							work behind the scenes. You can check out some examples of what
+							I've done in the Projects section.
+						</p>
+						<p>
+							I'm always on the lookout for new opportunities to put my skills
+							to work, learn new things, and grow as a developer. So if you've
+							got a cool project that you think I'd be a good fit for, feel free
+							to reach out and let's chat.
+						</p>
+						<Button className='w-52 py-6 shadow-gray-600 shadow-lg bg-violet-700 text-white rounded text-2xl'>
+							Contact
+						</Button>
+					</div>
+				</div>
+
+				{/* Right Column */}
+				<div className='flex flex-col items-center justify-self-center justify-top mt-4 space-y-4 mb-4 max-w-md'>
 					<div className='font-bold text-center text-3xl pt-6 lg:pt-0'>
 						My Skills
 					</div>
-					<div className='flex flex-wrap justify-center items-center gap-2 max-w-sm '>
+					<div className='flex flex-wrap justify-center items-center gap-2 max-w-sm'>
 						<Chip className='bg-neutral-800 text-white rounded-lg text-xl px-2 py-4'>
 							HTML
 						</Chip>
