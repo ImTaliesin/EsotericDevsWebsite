45e3bb1c5c227740df1be3ca0aa47333c30be504
ImTaliesin <110573773+ImTaliesin@users.noreply.github.com>
2024-03-11 15:14:26 -0700

    Add Footer component and adjust background color in About section

---
+++ src/components/UI/Footer.tsx
@@ -0,0 +1,11 @@
+
+export default function Footer() {
+  return (
+    <footer className='z-100 bg-neutral-950 text-white text-center py-4'>
+      <div className="container mx-auto px-4 space-y-1">
+        <p>&copy; 2024 EsotericDevs | Clovis, CA | All rights reserved </p>
+        <p>Email: Taliesin@Esotericdevs.com</p>
+      </div>
+    </footer>
+  );
+}
