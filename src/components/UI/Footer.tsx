3fdc206fa0bcb54680445146c9d7072e6abb28d6
ImTaliesin <110573773+ImTaliesin@users.noreply.github.com>
2024-03-31 23:01:57 -0700

    Refactor page layout and add blur effect to footer

--- src/components/UI/Footer.tsx
+++ src/components/UI/Footer.tsx
@@ -1,11 +1,13 @@
-
+import BlurSection from '../Small/blurSection';
 export default function Footer() {
-  return (
-    <footer className='z-100 bg-neutral-950 text-white text-center py-4'>
-      <div className="container mx-auto px-4 space-y-1">
-        <p>&copy; 2024 EsotericDevs | Clovis, CA | All rights reserved </p>
-        <p>Email: Taliesin@Esotericdevs.com</p>
-      </div>
-    </footer>
-  );
+	return (
+		<BlurSection>
+			<footer className='z-100 text-white text-center mt-4'>
+				<div className='container mx-auto px-4 space-y-1'>
+					<p>&copy; 2024 EsotericDevs | Clovis, CA | All rights reserved </p>
+					<p>Email: Taliesin@Esotericdevs.com</p>
+				</div>
+			</footer>
+		</BlurSection>
+	);
 }
