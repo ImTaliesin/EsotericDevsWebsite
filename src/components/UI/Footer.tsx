74e5300c347a92d5d78179fd9e8191fb8445e611
ImTaliesin <110573773+ImTaliesin@users.noreply.github.com>
2024-04-16 21:55:03 -0700

    Update footer margin in Footer.tsx

--- src/components/UI/Footer.tsx
+++ src/components/UI/Footer.tsx
@@ -2,7 +2,7 @@ import BlurSection from '../Small/blurSection';
 export default function Footer() {
 	return (
 		<BlurSection>
-			<footer className='z-100 text-white text-center mt-4'>
+			<footer className='z-100 text-white text-center my-4'>
 				<div className='container mx-auto px-4 space-y-1'>
 					<p>&copy; 2024 EsotericDevs | Clovis, CA | All rights reserved </p>
 					<p>Email: Taliesin@Esotericdevs.com</p>
