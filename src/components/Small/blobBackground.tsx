f1b54d4823ba37fad3fed21c4cdb8464b9033c1a
ImTaliesin <110573773+ImTaliesin@users.noreply.github.com>
2024-06-28 22:03:28 -0700

    its fucked

--- src/components/Small/blobBackground.tsx
+++ src/components/Small/blobBackground.tsx
@@ -1,45 +1,56 @@
-'use client'
+'use client';
 import './style.scss';
 import { useEffect, useRef } from 'react';
 
 interface BlobBackgroundProps {
-  className?: string;
+	className?: string;
 }
 
-const BlobBackground: React.FC<BlobBackgroundProps> = ({ className = '', ...props }) => {
-  const interBubbleRef = useRef<HTMLDivElement>(null);
+const BlobBackground: React.FC<BlobBackgroundProps> = ({
+	className = '',
+	...props
+}) => {
+	const interBubbleRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    let curX = 0;
-    let curY = 0;
-    let tgX = 0;
-    let tgY = 0;
+	useEffect(() => {
+		let curX = 0;
+		let curY = 0;
+		let tgX = 0;
+		let tgY = 0;
 
-    const move = () => {
-      curX += (tgX - curX) / 20;
-      curY += (tgY - curY) / 20;
+		const move = () => {
+			curX += (tgX - curX) / 20;
+			curY += (tgY - curY) / 20;
 
-      if (interBubbleRef.current) {
-        interBubbleRef.current.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
-      }
+			if (interBubbleRef.current) {
+				interBubbleRef.current.style.transform = `translate(${Math.round(
+					curX
+				)}px, ${Math.round(curY)}px)`;
+			}
 
-      requestAnimationFrame(move);
-    };
+			requestAnimationFrame(move);
+		};
 
-    const handleMouseMove = (event: MouseEvent) => {
-      tgX = event.clientX;
-      tgY = event.clientY;
-    };
+		const handleMouseMove = (event: MouseEvent) => {
+			tgX = event.clientX;
+			tgY = event.clientY;
+		};
 
-    window.addEventListener('mousemove', handleMouseMove);
-    move();
+		window.addEventListener('mousemove', handleMouseMove);
+		move();
 
-    return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, []);
+		return () => {
+			window.removeEventListener('mousemove', handleMouseMove);
+		};
+	}, []);
 
-  return <div className={`interactive z-10 ${className}`} ref={interBubbleRef} {...props} />;
+	return (
+		<div
+			className={`interactive z-10 ${className}`}
+			ref={interBubbleRef}
+			{...props}
+		/>
+	);
 };
 
 export default BlobBackground;
