ab4978cb536b836db8b9af3678d7eca015d48bdf
Taliesin <110573773+ImTaliesin@users.noreply.github.com>
2024-07-01 13:38:33 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,36 @@
-'use client';
 import AboutMe from '@/components/UI/AboutMe';
 import HeroSection from '@/components/UI/Hero';
 import Projects from '@/components/UI/Projects';
 import Background from '@/components/Small/backgroundSection';
 import Contact from '@/components/UI/Contact';
 import Footer from '@/components/UI/Footer';
-import Reveal from '@/components/UI/Reveal';
 
 export default function Home() {
 	return (
-		<div>
+		<div className='bg-neutral-950'>
 			<Background />
 
-			<HeroSection />
+			<main>
+				<HeroSection />
 
-			<div className='bg-neutral-950'>
-				<div id='About'>
+				<section id='About'>
 					<AboutMe />
-				</div>
+				</section>
 
-				<div
+				<section
 					id='Projects'
 					className='mt-4'>
 					<Projects />
-				</div>
+				</section>
 
-				<div
+				<section
 					id='Contact'
 					className='mt-4'>
 					<Contact />
-				</div>
+				</section>
+			</main>
 
-				<div>
-					<Footer />
-				</div>
-			</div>
+			<Footer />
 		</div>
 	);
 }
