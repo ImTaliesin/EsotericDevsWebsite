42d8078c8392c9e849839ffc7b690161dddd9665
Taliesin <110573773+ImTaliesin@users.noreply.github.com>
2024-07-04 14:18:42 -0700

    Big improvements

--- src/components/UI/AboutMe.tsx
+++ src/components/UI/AboutMe.tsx
@@ -1,9 +1,13 @@
 'use client';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import BlurSection from '@/components/Small/blurSection';
-import { Chip, Button } from '@nextui-org/react';
+import { Chip } from '@nextui-org/react';
+import Reveal from '@/components/UI/Reveal';
 
-const handleAnchorClick = (e: any, anchorId: string) => {
+const handleAnchorClick = (
+	e: React.MouseEvent<HTMLButtonElement>,
+	anchorId: string
+) => {
 	e.preventDefault();
 	const element = document.getElementById(anchorId);
 	if (element) {
@@ -11,77 +15,126 @@ const handleAnchorClick = (e: any, anchorId: string) => {
 	}
 };
 
-const AboutMe = () => {
+interface ScrollingSkillsProps {
+	skills: string[];
+}
+
+const ScrollingSkills = ({ skills }: ScrollingSkillsProps) => {
+	const [scrollPosition, setScrollPosition] = useState(0);
+
+	useEffect(() => {
+		const totalWidth = skills.length * 150; // Assuming each skill chip is about 150px wide
+		const intervalId = setInterval(() => {
+			setScrollPosition((prevPosition) => {
+				//scroll
+				const newPosition = prevPosition + 1;
+				// If we've scrolled past all skills, reset to the beginning
+				return newPosition >= totalWidth ? 0 : newPosition;
+			});
+		}, 40); // Adjust speed as needed
+		// Clean up the interval on component unmount
+		return () => clearInterval(intervalId);
+	}, [skills]);
+
+	return (
+		<div
+			className='relative overflow-hidden'
+			style={{ height: '80px' }}>
+			<div
+				className='absolute top-0 left-0 w-96 h-16 bg-gradient-to-r from-indigo-900/40 to-transparent z-10'
+				style={{ mixBlendMode: 'multiply' }}></div>
+			<div
+				className='absolute top-0 right-0 w-96 h-16 bg-gradient-to-l from-indigo-900/40 to-transparent z-10'
+				style={{ mixBlendMode: 'multiply' }}></div>
+			<div
+				className='flex whitespace-nowrap py-4'
+				style={{ transform: `translateX(-${scrollPosition}px)` }}>
+				{[...skills, ...skills].map((skill, index) => (
+					<Chip
+						key={`${skill}-${index}`}
+						className='bg-violet-700 text-white text-lg px-4 py-2 mx-2'>
+						{skill}
+					</Chip>
+				))}
+			</div>
+		</div>
+	);
+};
+
+const AboutMe: React.FC = () => {
+	const skills = [
+		'HTML',
+		'CSS',
+		'TailwindCSS',
+		'JavaScript',
+		'TypeScript',
+		'ReactJS',
+		'NextJS',
+		'NodeJS',
+		'Framer Motion',
+		'GIT',
+		'Docker',
+	];
+
 	return (
 		<BlurSection>
-			<div className='max-w-4xl mx-auto px-4 py-16'>
-				<h1 className='font-sans text-4xl font-bold text-center mb-8'>
-					About Me
-				</h1>
+			<div className='max-w-4xl mx-auto px-4 py-16 flex flex-col items-center'>
+				<Reveal>
+					<h1 className='font-sans text-4xl font-bold text-center mb-8'>
+						About Me{' '}
+						<div className='relative mt-6 mb-2 h-1 w-24 mx-auto bg-violet-600 rounded-full'></div>
+					</h1>
+				</Reveal>
+				<div className='w-full space-y-8'>
+					<Reveal delay={0.2}>
+						<div className='bg-indigo-900 rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-20 border border-gray-100/30 p-6 mb-8 shadow-lg'>
+							<p className='text-lg leading-relaxed'>
+								Hey there! I'm a web developer with a passion for tinkering with
+								code. When I'm not working on websites, or learning new
+								technologies, I usually mod videogames and host private servers
+								for communities to play on them. Currently, I'm working on an
+								exciting project for a new animation studio, bringing their
+								creative vision to life on the web.
+							</p>
+						</div>
+					</Reveal>
 
-				<div className='bg-indigo-900 rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-20 border border-gray-100/30
- p-6 mb-8 shadow-lg'>
-					<p className='text-lg leading-relaxed'>
-						Hi there! I'm a web developer who loves building cool stuff with
-						code. I'm always eager to learn new technologies and improve my
-						skills. I'm currently working on a website for a new animation studio.
-					</p>
-				</div>
-
-				<div className='grid md:grid-cols-2 gap-8 mb-8'>
-					<div className='bg-indigo-900 rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-20 border border-gray-100/30
- p-6 shadow-lg'>
-						<h2 className='text-2xl font-semibold mb-4'>Experience</h2>
-						<p className='text-lg leading-relaxed'>
-							I've worked on all sorts of projects, from front-end interfaces to
-							back-end systems. My experience spans across various technologies
-							and frameworks, allowing me to build comprehensive web solutions.
-							Check out my Projects section for examples of my work.
-						</p>
+					<div className='grid md:grid-cols-2 gap-8 mb-8'>
+						<Reveal delay={0.4}>
+							<div className='bg-indigo-900 rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-20 border border-gray-100/30 p-6 shadow-lg'>
+								<h2 className='text-2xl font-semibold mb-4'>Experience</h2>
+								<p className='text-lg leading-relaxed'>
+									My experience spans a wide range of web development areas,
+									from creating user-friendly front-end interfaces to developing
+									intricate back-end systems. If you're interested in seeing
+									examples of my work, please check out my Projects section.
+								</p>
+							</div>
+						</Reveal>
+						<Reveal delay={0.6}>
+							<div className='bg-indigo-900 rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-20 border border-gray-100/30 p-6 shadow-lg'>
+								<h2 className='text-2xl font-semibold mb-4'>Aspirations</h2>
+								<p className='text-lg leading-relaxed'>
+									I'm always eager to take on new challenges that will expand my
+									skill set. Whether it's learning a new programming language or
+									solving complex problems, I enjoy pushing my boundaries. If
+									you have an exciting project in mind that could benefit from
+									my expertise, don't hesitate to reach out!
+								</p>
+							</div>
+						</Reveal>
 					</div>
-					<div className='bg-indigo-900 rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-20 border border-gray-100/30
- p-6 shadow-lg'>
-						<h2 className='text-2xl font-semibold mb-4'>Aspirations</h2>
-						<p className='text-lg leading-relaxed'>
-							I'm always on the lookout for new opportunities to apply my
-							skills, learn new things, and grow as a developer. If you have an
-							exciting project that could benefit from my expertise, I'd love to
-							hear about it!
-						</p>
-					</div>
-				</div>
 
-				<div className='bg-indigo-900 rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-20 border border-gray-100/30
- p-6 shadow-lg mb-8'>
-					<h2 className='text-2xl font-semibold mb-4 text-center'>My Skills</h2>
-					<div className='flex flex-wrap justify-center gap-3'>
-
-						{/*TODO: Change array to photo carousel */}
-						{[
-							'HTML',
-							'CSS',
-							'JavaScript',
-							'ReactJS',
-							'NextJS',
-							'NodeJS',
-							'GIT',
-							'SEO',
-						].map((skill) => (
-							<Chip
-								key={skill}
-								className='bg-violet-700 text-white text-lg px-4 py-2'>
-								{skill}
-							</Chip>
-						))}
-					</div>
-				</div>
-
-				<div className='text-center'>
-					<Button
-						onClick={(e) => handleAnchorClick(e, 'Contact')}
-						className='px-8 py-4 bg-violet-700 text-white rounded-full text-xl shadow-lg hover:bg-violet-600 transition-colors'>
-						Let's Connect
-					</Button>
+					<Reveal
+						delay={0.8}
+						width='100%'>
+						<div className='bg-indigo-900 rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-20 border border-gray-100/30 py-6 shadow-lg mb-2'>
+							<h2 className='text-2xl font-semibold mb-4 text-center'>
+								My Digital Toolbox
+							</h2>
+							<ScrollingSkills skills={skills} />
+						</div>
+					</Reveal>
 				</div>
 			</div>
 		</BlurSection>
--- src/components/UI/Projects.tsx
+++ src/components/UI/Projects.tsx
@@ -1,6 +1,9 @@
+'use client';
+import React from 'react';
 import Link from 'next/link';
 import BlurSection from '../Small/blurSection';
 import Image from 'next/image';
+import Reveal from '@/components/UI/Reveal';
 
 export default function Projects() {
 	return (
@@ -8,48 +11,56 @@ export default function Projects() {
 			<div
 				id='Projects'
 				className='flex m-14 flex-col justify-top items-center'>
-				<h1 className='font-sans text-4xl font-bold'>Projects</h1>
-				<div className='relative mt-4 mb-2 h-1 w-12 bg-violet-600 rounded-full'></div>
+				<Reveal>
+					<h1 className='font-sans text-4xl font-bold'>Projects</h1>
+					<div className='relative mt-4 mb-1 h-1 w-24 mx-auto bg-violet-600 rounded-full'></div>
+				</Reveal>
+
 				{/* Brew Haus */}
-				<div className='flex flex-col md:flex-row items-center'>
-					<Link href='https://talie-restaurant-template.vercel.app/'>
-						<Image
-							alt='Laptop Image'
-							src='/BrewHausLaptop.png'
-							width={500}
-							height={300}
-							className='flex-1'
-						/>
-					</Link>
-					<div className='md:ml-2 max-w-md mt-6 md:mt-0'>
-						<p className='text-xl text-justify'>
-							Brew Haus is a modern restaurant template built using Next.js and
-							Tailwind CSS that I designed and developed. This website showcases
-							a responsive layout that adapts seamlessly to different screen
-							sizes, ensuring an optimal viewing experience across all desktop
-							and mobile screens.
-						</p>
+				<Reveal delay={0.2}>
+					<div className='flex flex-col md:flex-row items-center'>
+						<Link href='https://talie-restaurant-template.vercel.app/'>
+							<Image
+								alt='Laptop Image'
+								src='/BrewHausLaptop.png'
+								width={500}
+								height={300}
+								className='flex-1'
+							/>
+						</Link>
+						<div className='md:ml-2 max-w-md mt-6 md:mt-0'>
+							<p className='text-xl text-justify'>
+								Brew Haus is a modern restaurant template built using Next.js
+								and Tailwind CSS that I designed and developed. This website
+								showcases a responsive layout that adapts seamlessly to
+								different screen sizes, ensuring an optimal viewing experience
+								across all desktop and mobile screens.
+							</p>
+						</div>
 					</div>
-				</div>
+				</Reveal>
+
 				{/* Store Citizen */}
-				<div className='flex flex-col md:flex-row items-center mt-8'>
-					<Link href='https://chromewebstore.google.com/detail/store-citizen/kedcjkkkabjjhbadhabaikbpcdodhopl?pli=1'>
-						<Image
-							alt='Laptop Image'
-							src='/StoreCitizenLaptop.png'
-							width={500}
-							height={300}
-							className='flex-1'
-						/>
-					</Link>
-					<div className='md:ml-2 max-w-md mt-6 md:mt-0'>
-						<p className='text-xl text-justify'>
-							Store Citizen is a chrome extension that quickly and easily shows
-							you which ships are currently on sale in the Star Citizen store.
-							This extension is built using React and Tailwind CSS.
-						</p>
+				<Reveal delay={0.4}>
+					<div className='flex flex-col md:flex-row items-center mt-8'>
+						<Link href='https://chromewebstore.google.com/detail/store-citizen/kedcjkkkabjjhbadhabaikbpcdodhopl?pli=1'>
+							<Image
+								alt='Laptop Image'
+								src='/StoreCitizenLaptop.png'
+								width={500}
+								height={300}
+								className='flex-1'
+							/>
+						</Link>
+						<div className='md:ml-2 max-w-md mt-6 md:mt-0'>
+							<p className='text-xl text-justify'>
+								Store Citizen is a chrome extension that quickly and easily
+								shows you which ships are currently on sale in the Star Citizen
+								store. This extension is built using React and Tailwind CSS.
+							</p>
+						</div>
 					</div>
-				</div>
+				</Reveal>
 			</div>
 		</BlurSection>
 	);
